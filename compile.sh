21334e0e090c6ded77cedac1cdc3443144a0a3ad
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2024-12-31 07:13:38 +0800

    Build with ksu-next and susfs4ksu

--- compile.sh
+++ compile.sh
@@ -8,13 +8,17 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+rmdir KernelSU
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs-4.9/kernel/setup.sh" | bash -s next-susfs-4.9
+
+git clone https://gitlab.com/simonpunk/susfs4ksu.git --depth 1 --branch kernel-4.9
+cp -r susfs4ksu/kernel_patches/* .
+patch -p1 < 50_add_susfs_in_kernel-4.9.patch
 
 # Cleanup
 rm -rf out/outputs/*
 
 ./compile-beryllium.sh
-./compile-dipper.sh
 
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
